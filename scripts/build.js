332068f050ea0275f67cc53062aa1c8d94c36250
orangemug <info@orangemug.co.uk>
2016-09-05 16:04:54 +0100

    Use colors in app.scss

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 var fs         = require("fs");
 var Handlebars = require("handlebars");
 var colors     = require("../");
+var sass = require('node-sass');
 
 
 function tmpl(inpath, obj, outpath) {
@@ -17,14 +18,24 @@ function tmpl(inpath, obj, outpath) {
   });
 }
 
-var css = fs.readFileSync(__dirname+"/app.css").toString();
+function renderSass() {
+  return new Promise(function(resolve, reject) {
+    sass.render({file: __dirname+"/app.scss"}, function(err, data) {
+      if(err) return reject(err);
 
-Promise
-  .all([
-    tmpl(__dirname+"/../templates/scss.hbs", {colors: colors.obj}, __dirname+"/../index.scss"),
-    tmpl(__dirname+"/../templates/docs.hbs", {colors: colors.obj, css: css}, __dirname+"/../docs/index.html"),
-    // tmpl(__dirname+"/../templates/less.hbs", {colors: colors.obj}, __dirname+"/../index.less")
-  ])
+      resolve(data.css.toString());
+    });
+  });
+}
+
+renderSass()
+  .then(function(css) {
+    return Promise
+      .all([
+        tmpl(__dirname+"/../templates/scss.hbs", {colors: colors.obj}, __dirname+"/../index.scss"),
+        tmpl(__dirname+"/../templates/docs.hbs", {colors: colors.obj, css: css}, __dirname+"/../docs/index.html")
+      ])
+  })
   .then(function() {
     process.exit(0);
   })
